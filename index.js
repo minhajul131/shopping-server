dd54ab00c7f39df23d41c68e0593b7a6d78e2eaf
Minhajul Islam <36797565+minhajul131@users.noreply.github.com>
2022-07-31 11:15:49 +0600

    updated again

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 // const MongoClient = require('mongodb').MongoClient;
 
-const uri = `mongodb+srv://shopping:shopping123@cluster0.lw8h6rv.mongodb.net/shoppingfull?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lw8h6rv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const app = express()
 console.log("the :" ,uri)
 
